31c030427c235bc6e230c2ddbbb606660285f130
Marlon C.S <marlsas0306@gmail.com>
2020-04-04 15:05:36 -0500

    updated user functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Observable, empty } from 'rxjs';
+import { map, shareReplay, switchMap, mergeMap } from 'rxjs/operators';
+import { UserService } from './_services/user.service';
+import { SpotifyService } from './_services/spotify.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spotify-api-test';
   isLogged = false;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -16,6 +18,17 @@ export class AppComponent {
     map(result => result.matches),
     shareReplay()
   );
+  userInfo: any;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              private userService: UserService,
+              private spotifyService: SpotifyService) {}
+
+  ngOnInit() {
+    this.spotifyService
+        .currentToken.pipe(
+         switchMap((token) => token ? this.userService.getUser() : empty())
+        )
+        .subscribe(user => this.userInfo = user);
+  }
 }
