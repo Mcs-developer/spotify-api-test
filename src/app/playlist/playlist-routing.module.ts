cbf652738a981fe32e73ccf71d3328e2273e6aa0
Marlon C.S <marlsas0306@gmail.com>
2020-04-05 18:31:41 -0500

    wrapped playlist component

--- src/app/playlist/playlist-routing.module.ts
+++ src/app/playlist/playlist-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlaylistLayoutComponent } from './playlist-layout/playlist-layout.component';
+import { PlaylistListComponent } from './playlist-list/playlist-list.component';
 
 
 const routes: Routes = [
-  { path: '', component: PlaylistLayoutComponent }
+  { 
+    path: '', component: PlaylistLayoutComponent,
+    children: [
+      { path: '', component: PlaylistListComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/playlist/playlist.module.ts
+++ src/app/playlist/playlist.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { PlaylistRoutingModule } from './playlist-routing.module';
 import { PlaylistLayoutComponent } from './playlist-layout/playlist-layout.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { PlaylistListComponent } from './playlist-list/playlist-list.component';
 
 @NgModule({
-  declarations: [PlaylistLayoutComponent],
+  declarations: [PlaylistLayoutComponent, PlaylistListComponent],
   imports: [
     CommonModule,
-    PlaylistRoutingModule
+    PlaylistRoutingModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class PlaylistModule { }
