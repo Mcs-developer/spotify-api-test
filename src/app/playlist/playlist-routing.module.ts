b7efcd2725345365d36d057c6c378b9b6cda0965
Marlon C.S <marlsas0306@gmail.com>
2020-04-05 23:08:35 -0500

    wrapped components

--- src/app/playlist/playlist-routing.module.ts
+++ src/app/playlist/playlist-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlaylistLayoutComponent } from './playlist-layout/playlist-layout.component';
 import { PlaylistListComponent } from './playlist-list/playlist-list.component';
+import { PlaylistDetailComponent } from './playlist-detail/playlist-detail.component';
+import { PlaylistNewComponent } from './playlist-new/playlist-new.component';
 
 
 const routes: Routes = [
   { 
     path: '', component: PlaylistLayoutComponent,
     children: [
-      { path: '', component: PlaylistListComponent }
+      { path: '', component: PlaylistListComponent },
+      { path: 'detail/:id', component: PlaylistDetailComponent },
+      { path: 'new', component: PlaylistNewComponent }
     ]
   }
 ];
--- src/app/playlist/playlist.module.ts
+++ src/app/playlist/playlist.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PlaylistRoutingModule } from './playlist-routing.module';
 import { PlaylistLayoutComponent } from './playlist-layout/playlist-layout.component';
+import { PlaylistListComponent } from './playlist-list/playlist-list.component';
+import { PlaylistDetailComponent } from './playlist-detail/playlist-detail.component';
+import { PlaylistNewComponent } from './playlist-new/playlist-new.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { PlaylistListComponent } from './playlist-list/playlist-list.component';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+
 
 @NgModule({
-  declarations: [PlaylistLayoutComponent, PlaylistListComponent],
+  declarations: [PlaylistLayoutComponent, PlaylistListComponent, PlaylistDetailComponent, PlaylistNewComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     PlaylistRoutingModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule, 
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class PlaylistModule { }
