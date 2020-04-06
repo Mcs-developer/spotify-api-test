e3315b1d2fa6b5905482099d6a194759e927f82d
Marlon C.S <marlsas0306@gmail.com>
2020-04-05 23:08:11 -0500

    added functionality delete and unfollow

--- src/app/playlist/playlist-list/playlist-list.component.ts
+++ src/app/playlist/playlist-list/playlist-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlaylistService } from 'src/app/_services/playlist.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-playlist-list',
@@ -10,31 +11,40 @@ export class PlaylistListComponent implements OnInit {
 
   playlist;
 
-  constructor(private playlistService: PlaylistService) { }
+  constructor(private playlistService: PlaylistService,
+              private router: Router) { }
 
   ngOnInit(): void {
-
-    this.playlistService
-        .getPlaylist()
-        .subscribe(playlist => {
-          this.playlist = playlist['items'];
-        });
-    
+    this.getPlaylist();
   }
+
+  getPlaylist() {
+    this.playlistService
+    .getPlaylist()
+    .subscribe(playlist => {
+      this.playlist = playlist['items'];
+    });
+  } 
 
   detailList(id) {
-    console.log(id);
+    this.router.navigate(['playlist','detail',id]);
   }
 
-  deletePlaylist(id, event) {
+  unfollowPlaylist(playlist_id, event) {
     event.stopPropagation();
-    console.log(id);
+    const deletePlaylist = confirm('Are you sure to delete playlist?');
+    if(deletePlaylist == true) {
+      this.playlistService
+        .unfollowPlaylist(playlist_id)
+        .subscribe(() => this.getPlaylist());
+    } 
+    
   }
 
   showImage(images) {
     if(images.length) {
       return images.length > 1 ? images[1].url : images[0].url;
     }
-    return '';
+    return 'assets/no_album.png';
   }
 }
