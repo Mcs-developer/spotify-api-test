c313468a96ae696045f919e829c4a85aa3934394
Marlon C.S <marlsas0306@gmail.com>
2020-04-05 23:03:00 -0500

    Added methods to playlist services

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
 
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<any>(null);
+    this.currentUser = this.currentUserSubject.asObservable();
+   }
 
   getUser() {
-    return this.http.get('https://api.spotify.com/v1/me');
+    return this.http.get('https://api.spotify.com/v1/me').pipe(tap(user => this.currentUserSubject.next(user)));
   }
 }
