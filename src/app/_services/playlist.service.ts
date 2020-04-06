72feb670ccbbf2438b73e334d089e2b4238888ff
Marlon C.S <marlsas0306@gmail.com>
2020-04-05 23:03:35 -0500

    added some methods to playlist services

--- src/app/_services/playlist.service.ts
+++ src/app/_services/playlist.service.ts
@@ -11,4 +11,17 @@ export class PlaylistService {
   getPlaylist() {
     return this.http.get('https://api.spotify.com/v1/me/playlists');
   }
+
+  getDetailPlayList(playlist_id) {
+    const fields = 'fields=name,description,tracks.items(track(name,album(name,images), artists(name)))'
+    return this.http.get(`https://api.spotify.com/v1/playlists/${playlist_id}?${fields}`);
+  }
+
+  createPlaylist(user_id, body) {
+    return this.http.post(`https://api.spotify.com/v1/users/${user_id}/playlists`, body);
+  }
+
+  unfollowPlaylist(playlist_id) {
+    return this.http.delete(`https://api.spotify.com/v1/playlists/${playlist_id}/followers`);
+  }
 }
